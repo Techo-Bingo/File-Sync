b1aea7a6de255172e429caebe47499ef07217173
Bingo <linux_bingo@163.com>
2019-12-04 18:52:40 +0800

    Update fs_logger.py

--- fs_logger.py
+++ fs_logger.py
@@ -68,18 +68,15 @@ class LogTrunc:
         Logger.info("[fs_logger] Trunk log: %s" % Global.G_LOG_FILE)
         # 获取去除.log后的日志文件前缀
         name = '.'.join(Global.G_LOG_FILE.split('.')[:-1])
-        # 压缩当前进程日志并限制压缩包个数
+        # 回滚当前进程日志
         Common.shell_cmd("cp {0} {0}.1 && > {0} && "
                          "tar zcvf {1}_$(date +'%Y%m%d-%H%M').tar.gz {0}.1 && "
-                         "rm {0}.1;ls -t {1}_*.tar.gz|sed -n '15,100p'|xargs rm -rf"
-                         .format(Global.G_LOG_FILE, name))
+                         "rm {0}.1".format(Global.G_LOG_FILE, name))
 
     @classmethod
     def keep_count(cls):
-        # TODO 如果日志对应pid的filesync正在运行，则不能删除该日志；
-        # 当前需求中不可能超过10个文件同步同时运行，则先默认值保留10个.
-        Common.shell_cmd("ls -t %s/*.log|sed -n '10,1000p'|xargs rm -rf"
-                         % Global.G_LOG_DIR)
+        Common.shell_cmd("ls -t %s/*.gz|sed -n '%s,1000p'|xargs rm -rf"
+                         % (Global.G_LOG_DIR, Global.G_MAX_COUNT))
 
     @classmethod
     def rollback(cls, args=None):
