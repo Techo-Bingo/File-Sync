457e037098de3134f217671f1a04cb023292a8fa
Bingo <linux_bingo@163.com>
2021-01-16 15:30:24 +0800

    Update ini_operator.py

--- tools/ini_operator.py
+++ tools/ini_operator.py
@@ -7,6 +7,83 @@ else:
     import configparser as ConfigParser
 
 
+# 去除python ConfigParser 库option字段被改小写的问题 #
+class _MyConfigParser(ConfigParser.ConfigParser):
+    def __init__(self, defaults=None):
+        ConfigParser.ConfigParser.__init__(self, defaults=defaults)
+
+    def optionxform(self, optionstr):
+        return optionstr
+
+
+class IniParser(object):
+
+    def __init__(self, conf_path):
+        self.conf_path = conf_path
+        self.conf = _MyConfigParser()
+
+    def get_all_sections(self):
+        try:
+            self.conf.read(self.conf_path)
+            return self.conf.sections(), None
+        except Exception as e:
+            return None, str(e)
+
+    def get_section_options(self, section):
+        try:
+            self.conf.read(self.conf_path)
+            return self.conf.options(section), None
+        except Exception as e:
+            return None, str(e)
+
+    def get_section_items(self, section):
+        try:
+            self.conf.read(self.conf_path)
+            return self.conf.items(section), None
+        except Exception as e:
+            return None, str(e)
+
+    def get_value(self, section, option):
+        try:
+            self.conf.read(self.conf_path)
+            return self.conf.get(section, option), None
+        except Exception as e:
+            return None, str(e)
+
+    def add_section(self, section):
+        try:
+            sections, _ = self.get_all_sections()
+            if section not in sections:
+                self.conf.add_section(section)
+                self.conf.write(open(self.conf_path, "w"))
+                return True, None
+        except Exception as e:
+            return False, str(e)
+
+    def del_section(self, section):
+        try:
+            sections, _ = self.get_all_sections()
+            if section in sections:
+                self.conf.remove_section(section)
+                self.conf.write(open(self.conf_path, "w"))
+            return True, None
+        except Exception as e:
+            return False, str(e)
+
+    def set_value(self, section, option, value):
+        try:
+            sections, err = self.get_all_sections()
+            if not sections:
+                raise Exception(err)
+            if section not in sections:
+                raise Exception('section: {0} is not exist'.format(section))
+            self.conf.set(section, option, value)
+            self.conf.write(open(self.conf_path, "w"))
+            return True, None
+        except Exception as e:
+            return False, str(e)
+
+
 def usage(oper=None):
     _use = {'get_sections': 'python %s get_sections <INIPATH>' % sys.argv[0],
             'add_section': 'python %s add_section <INIPATH> <SECTION>' % sys.argv[0],
@@ -26,67 +103,6 @@ def sys_err(msg):
     sys.exit(1)
 
 
-class Parser(object):
-    """ 实时读取或修改配置文件的值 """
-    def __init__(self, conf_path):
-        self.conf_path = conf_path
-        self.conf = ConfigParser.ConfigParser()
-
-    def get_all_sections(self):
-        try:
-            self.conf.read(self.conf_path)
-            return self.conf.sections()
-        except Exception as e:
-            sys_err(e)
-
-    def get_section_options(self, section):
-        try:
-            self.conf.read(self.conf_path)
-            return self.conf.options(section)
-        except Exception as e:
-            sys_err(e)
-
-    def get_section_items(self, section):
-        try:
-            self.conf.read(self.conf_path)
-            return self.conf.items(section)
-        except Exception as e:
-            sys_err(e)
-
-    def get_value(self, section, option):
-        try:
-            self.conf.read(self.conf_path)
-            return self.conf.get(section, option)
-        except Exception as e:
-            sys_err(e)
-
-    def add_section(self, section):
-        try:
-            if section not in self.get_all_sections():
-                self.conf.add_section(section)
-                self.conf.write(open(self.conf_path, "w"))
-        except Exception as e:
-            sys_err(e)
-
-    def del_section(self, section):
-        try:
-            if section in self.get_all_sections():
-                self.conf.remove_section(section)
-                self.conf.write(open(self.conf_path, "w"))
-        except Exception as e:
-            sys_err(e)
-
-    def set_value(self, section, option, value):
-        try:
-            if section in self.get_all_sections():
-                self.conf.set(section, option, value)
-                self.conf.write(open(self.conf_path, "w"))
-            else:
-                sys_err('section: %s is not exist' % section)
-        except Exception as e:
-            sys_err(e)
-
-
 def main():
     argv_size = len(sys.argv)
     if argv_size < 3:
@@ -95,9 +111,9 @@ def main():
     if not os.path.isfile(ini_file):
         sys_err('%s not exist' % ini_file)
 
-    parse = Parser(ini_file)
+    parse = IniParser(ini_file)
     if operate == 'get_sections':
-        print(' '.join(parse.get_all_sections()))
+        print(' '.join(parse.get_all_sections()[0]))
     elif operate == 'add_section':
         if argv_size != 4:
             sys_err(usage(operate))
@@ -109,11 +125,11 @@ def main():
     elif operate == 'get_options':
         if argv_size != 4:
             sys_err(usage(operate))
-        print(' '.join(parse.get_section_options(sys.argv[3])))
+        print(' '.join(parse.get_section_options(sys.argv[3])[0]))
     elif operate == 'get_value':
         if argv_size != 5:
             sys_err(usage(operate))
-        print(parse.get_value(sys.argv[3], sys.argv[4]))
+        print(parse.get_value(sys.argv[3], sys.argv[4])[0])
     elif operate == 'set_value':
         if argv_size != 6:
             sys_err(usage(operate))
